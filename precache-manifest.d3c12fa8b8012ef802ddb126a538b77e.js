0292cf95742cb5cb889327cdecf66fe199ae51a8
DDoS000 <Suphamongkhon.kh.61@ubu.ac.th>
2020-09-02 09:55:46 +0700

    Updates

---
+++ precache-manifest.d3c12fa8b8012ef802ddb126a538b77e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc01173780813a8fb8a359e37e4aa688",
+    "url": "/project-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f19b7f690e0f5b972c5e",
+    "url": "/project-tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "210717bcd6136923a012",
+    "url": "/project-tic-tac-toe/static/js/2.5ccea08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-tic-tac-toe/static/js/2.5ccea08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f19b7f690e0f5b972c5e",
+    "url": "/project-tic-tac-toe/static/js/main.22c15015.chunk.js"
+  },
+  {
+    "revision": "bd1ee2bf0938459e9dd6",
+    "url": "/project-tic-tac-toe/static/js/runtime-main.1b5f3697.js"
+  }
+]);
